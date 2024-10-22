79f46c89a3d8077fa17f20398048793ac484f820
WishperKnight <116265076+WishperKnight@users.noreply.github.com>
2024-10-22 14:50:43 +0200

    DataBaseHelper Ended

--- app/src/main/java/ies/carrillo/ejemploclasesqlite/database/DatabaseHelper.java
+++ app/src/main/java/ies/carrillo/ejemploclasesqlite/database/DatabaseHelper.java
@@ -1,19 +1,22 @@
 package ies.carrillo.ejemploclasesqlite.database;
 
+import android.content.Context;
+
 import androidx.annotation.NonNull;
 import androidx.room.InvalidationTracker;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-public class DatabaseHelper extends  RoomDatabase{
+public abstract class DatabaseHelper extends RoomDatabase {
 
-    @Override
-    public void clearAllTables() {
+    //Singleton
+    private static DatabaseHelper instance;
 
-    }
-
-    @NonNull
-    @Override
-    protected InvalidationTracker createInvalidationTracker() {
-        return null;
+    public static DatabaseHelper getInstance(Context context) {
+        //Si no se ha creado la bd, se crea
+        if (instance == null) {
+            instance = Room.databaseBuilder(context, DatabaseHelper.class, "ContactBook").fallbackToDestructiveMigration(true).build();
+        }
+        return instance;
     }
 }
