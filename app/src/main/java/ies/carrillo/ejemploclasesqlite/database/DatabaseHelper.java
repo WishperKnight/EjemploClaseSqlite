91fc70800fe4399743081a15539c9448e987a5c4
WishperKnight <116265076+WishperKnight@users.noreply.github.com>
2024-10-22 14:55:02 +0200

    DataBaseHelper Modified with annotation

--- app/src/main/java/ies/carrillo/ejemploclasesqlite/database/DatabaseHelper.java
+++ app/src/main/java/ies/carrillo/ejemploclasesqlite/database/DatabaseHelper.java
@@ -3,16 +3,21 @@ package ies.carrillo.ejemploclasesqlite.database;
 import android.content.Context;
 
 import androidx.annotation.NonNull;
+import androidx.room.Database;
 import androidx.room.InvalidationTracker;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+import ies.carrillo.ejemploclasesqlite.models.Person;
+
+@Database(entities= {Person.class},version = 1)
 public abstract class DatabaseHelper extends RoomDatabase {
 
     //Singleton
     private static DatabaseHelper instance;
 
-    public static DatabaseHelper getInstance(Context context) {
+    //synchronized es para que solo se pueda crear una instancia de la bd
+    public static synchronized DatabaseHelper getInstance(Context context) {
         //Si no se ha creado la bd, se crea
         if (instance == null) {
             instance = Room.databaseBuilder(context, DatabaseHelper.class, "ContactBook").fallbackToDestructiveMigration(true).build();
